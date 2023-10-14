f6607e383856c3e53c5f458b1b64670b95e795ac
Artem Fedotovskikh <artemfedotovskih69@gmail.com>
2023-10-14 16:03:55 +0500

    Popa

---
+++ works/js/index.js
@@ -0,0 +1,39 @@
+// let surname= "Федотовских";
+// let name= "Артём";
+// let patronomyc= "Денисович";
+// let ocupation="Студент";
+// let degree="Среднее професиональное";
+// let category="Первая";
+// let education="МПК";
+// let note="Информационные технологии";
+
+let info = [
+    {
+      surname: "Федотовских",
+      name: "Артём",
+      patronomyc: "Денисович",
+      ocupation: "Студент",
+      degree: "Среднее профессиональное",
+      category: "Первая",
+      education: "МПК",
+      note: "Информационные технологии",
+      note2:"Техника секса",
+      note3:"Физкультура",
+
+    }
+  ];
+
+
+//   let info = 
+//     {
+//       surname: "Федотовских",
+//       name: "Артём",
+//       patronomyc: "Денисович",
+//       ocupation: "Студент",
+//       degree: "Среднее профессиональное",
+//       category: "Первая",
+//       education: "МПК",
+//       note: "Информационные технологии"
+//     };
+
+   
